7ee73b60d8b4d06cb8d93a4f4e9ac58487c6e09b
Komugi <Shiira_C@outlook.jp>
2020-07-08 15:20:45 +0900

    Add counting words occurrences

--- rnn.py
+++ rnn.py
@@ -16,4 +16,20 @@ import numpy as np
 df = pd.read_csv('movie_data.csv')
 
 
+'''
+データの前処理；
+# 単語の分割をし，各単語の出現回数をカウント
+'''
+# 大量のデータセットから一意の単語を見つけるのに便利なcollectionsパッケージのCounterを読み込む
+from collections import Counter
+
+# Counterクラスからcountsオブジェクトを作成
+counts = Counter()
+# データの入力数でプログレスバーを初期化[終了時間計算用？]
+pbar = pyprind.ProgBar(len(df['review']), title='Counting words occurrences')
+for i,review in enumerate(df['review']):
+    text = ''.join([c if c not in punctuation else ' '+c+' ' for c in review]).lower()
+    df.loc()[i, 'review'] = text
+    pbar.update()
+    counts.update(text.split())
 
